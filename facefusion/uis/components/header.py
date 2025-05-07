aedce7c7e20b33dfee6855974e8e911187fe0bac
IrfaanS786biz <81854101+IrfaanS786biz@users.noreply.github.com>
2025-05-07 07:29:11 +0200

    ravenwatch

--- facefusion/uis/components/header.py
+++ facefusion/uis/components/header.py
@@ -4,7 +4,7 @@ def render() -> gr.Blocks:
 	with gr.Blocks():
 		gr.HTML("""
 			<h1 style="font-family: Arial, sans-serif; font-size: 28px; font-weight: bold; color: #4A90E2; text-align: center; padding: 20px;">
-				Offernet with FaceFusion Integration
+				RavenWatch with FaceFusion Integration
 			</h1>
 			<hr style="border: 2px solid #4A90E2; width: 80%; margin: auto;">
 		""")
